994498558d1236ccb3291107a558ef789ffe24ca
erfanamiriyazd <erfanamirij5@gmail.com>
2026-09-09 01:30:37 +0330

    Expose engineering run history through preload

--- src/desktop/preload/tolueBridge.ts
+++ src/desktop/preload/tolueBridge.ts
@@ -2,24 +2,20 @@ import type { EngineeringPdfExportRequest } from '../../engineering/core/enginee
 import type { SimulationRunInput } from '../../engineering/core/simulationRun';
 import { ENGINEERING_ANALYSIS_CHANNEL, type EngineeringAnalysisIpcResponse } from '../ipc/engineeringAnalysisIpc';
 import { ENGINEERING_PDF_EXPORT_CHANNEL, type EngineeringPdfIpcResponse } from '../ipc/engineeringPdfIpc';
-import { ENGINEERING_RUN_LOAD_CHANNEL, type EngineeringRunLoadIpcResponse } from '../ipc/engineeringRunIpc';
+import { ENGINEERING_RUN_HISTORY_CHANNEL, ENGINEERING_RUN_LOAD_CHANNEL, type EngineeringRunHistoryIpcResponse, type EngineeringRunLoadIpcResponse } from '../ipc/engineeringRunIpc';
 
 export interface TolueBridge {
   executeEngineeringAnalysis(input: SimulationRunInput): Promise<EngineeringAnalysisIpcResponse>;
   loadEngineeringRun(runId: string): Promise<EngineeringRunLoadIpcResponse>;
+  listEngineeringRuns(): Promise<EngineeringRunHistoryIpcResponse>;
   exportEngineeringPdf(request: EngineeringPdfExportRequest): Promise<EngineeringPdfIpcResponse>;
 }
 
 export function createTolueBridge(invoke: (channel: string, payload: unknown) => Promise<unknown>): Readonly<TolueBridge> {
   return Object.freeze({
-    executeEngineeringAnalysis: (input: SimulationRunInput) => invoke(ENGINEERING_ANALYSIS_CHANNEL, {
-      channel: ENGINEERING_ANALYSIS_CHANNEL, payload: input,
-    }) as Promise<EngineeringAnalysisIpcResponse>,
-    loadEngineeringRun: (runId: string) => invoke(ENGINEERING_RUN_LOAD_CHANNEL, {
-      channel: ENGINEERING_RUN_LOAD_CHANNEL, runId,
-    }) as Promise<EngineeringRunLoadIpcResponse>,
-    exportEngineeringPdf: (request: EngineeringPdfExportRequest) => invoke(ENGINEERING_PDF_EXPORT_CHANNEL, {
-      channel: ENGINEERING_PDF_EXPORT_CHANNEL, payload: request,
-    }) as Promise<EngineeringPdfIpcResponse>,
+    executeEngineeringAnalysis: (input: SimulationRunInput) => invoke(ENGINEERING_ANALYSIS_CHANNEL, { channel: ENGINEERING_ANALYSIS_CHANNEL, payload: input }) as Promise<EngineeringAnalysisIpcResponse>,
+    loadEngineeringRun: (runId: string) => invoke(ENGINEERING_RUN_LOAD_CHANNEL, { channel: ENGINEERING_RUN_LOAD_CHANNEL, runId }) as Promise<EngineeringRunLoadIpcResponse>,
+    listEngineeringRuns: () => invoke(ENGINEERING_RUN_HISTORY_CHANNEL, { channel: ENGINEERING_RUN_HISTORY_CHANNEL }) as Promise<EngineeringRunHistoryIpcResponse>,
+    exportEngineeringPdf: (request: EngineeringPdfExportRequest) => invoke(ENGINEERING_PDF_EXPORT_CHANNEL, { channel: ENGINEERING_PDF_EXPORT_CHANNEL, payload: request }) as Promise<EngineeringPdfIpcResponse>,
   });
 }
