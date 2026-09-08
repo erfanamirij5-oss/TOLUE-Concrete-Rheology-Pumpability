0a7fbe79859a03f84cdc995ab0f4d6721aae7ff6
erfanamiriyazd <erfanamirij5@gmail.com>
2026-09-08 23:01:49 +0330

    Wire renderer asset into desktop shell

--- src/desktop/main/index.ts
+++ src/desktop/main/index.ts
@@ -2,4 +2,7 @@ import { join } from 'node:path';
 import { startDesktopShell } from './desktopShell';
 
 // This entry is bundled as CommonJS; __dirname is owned by Electron Main.
-startDesktopShell(join(__dirname, '../preload/index.cjs'));
+startDesktopShell(
+  join(__dirname, '../preload/index.cjs'),
+  join(__dirname, '../renderer/index.js'),
+);
