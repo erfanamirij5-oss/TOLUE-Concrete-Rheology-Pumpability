bca2fd9344e7c722252dd9812859b3633f8c986d
erfanamiriyazd <erfanamirij5@gmail.com>
2026-09-11 21:45:03 +0330

    feat: expose verification evidence history bridge

--- src/desktop/preload/tolueBridge.ts
+++ src/desktop/preload/tolueBridge.ts
@@ -4,7 +4,14 @@ import { ENGINEERING_ANALYSIS_CHANNEL, type EngineeringAnalysisIpcResponse } fro
 import { ENGINEERING_PDF_EXPORT_CHANNEL, type EngineeringPdfIpcResponse } from '../ipc/engineeringPdfIpc';
 import { ENGINEERING_RUN_COMPARISON_CHANNEL, ENGINEERING_RUN_HISTORY_CHANNEL, ENGINEERING_RUN_LOAD_CHANNEL, type EngineeringRunComparisonIpcResponse, type EngineeringRunHistoryIpcResponse, type EngineeringRunLoadIpcResponse } from '../ipc/engineeringRunIpc';
 import { LICENSE_IMPORT_CHANNEL, LICENSE_STATUS_CHANNEL, type LicenseImportIpcResponse, type LicenseStatusIpcResponse } from '../ipc/licenseIpc';
-import { VERIFICATION_EVIDENCE_IMPORT_CHANNEL, type VerificationEvidenceImportIpcResponse } from '../ipc/verificationEvidenceIpc';
+import {
+  VERIFICATION_EVIDENCE_HISTORY_CHANNEL,
+  VERIFICATION_EVIDENCE_IMPORT_CHANNEL,
+  VERIFICATION_EVIDENCE_LOAD_CHANNEL,
+  type VerificationEvidenceHistoryIpcResponse,
+  type VerificationEvidenceImportIpcResponse,
+  type VerificationEvidenceLoadIpcResponse,
+} from '../ipc/verificationEvidenceIpc';
 
 export interface TolueBridge {
   executeEngineeringAnalysis(input: SimulationRunInput): Promise<EngineeringAnalysisIpcResponse>;
@@ -15,6 +22,8 @@ export interface TolueBridge {
   getLicenseStatus(): Promise<LicenseStatusIpcResponse>;
   importLicense(): Promise<LicenseImportIpcResponse>;
   importVerificationEvidence(): Promise<VerificationEvidenceImportIpcResponse>;
+  listVerificationEvidencePackages(): Promise<VerificationEvidenceHistoryIpcResponse>;
+  loadVerificationEvidencePackage(packageId: string): Promise<VerificationEvidenceLoadIpcResponse>;
 }
 
 export function createTolueBridge(invoke: (channel: string, payload: unknown) => Promise<unknown>): Readonly<TolueBridge> {
@@ -27,5 +36,7 @@ export function createTolueBridge(invoke: (channel: string, payload: unknown) =>
     getLicenseStatus: () => invoke(LICENSE_STATUS_CHANNEL, { channel: LICENSE_STATUS_CHANNEL }) as Promise<LicenseStatusIpcResponse>,
     importLicense: () => invoke(LICENSE_IMPORT_CHANNEL, { channel: LICENSE_IMPORT_CHANNEL }) as Promise<LicenseImportIpcResponse>,
     importVerificationEvidence: () => invoke(VERIFICATION_EVIDENCE_IMPORT_CHANNEL, { channel: VERIFICATION_EVIDENCE_IMPORT_CHANNEL }) as Promise<VerificationEvidenceImportIpcResponse>,
+    listVerificationEvidencePackages: () => invoke(VERIFICATION_EVIDENCE_HISTORY_CHANNEL, { channel: VERIFICATION_EVIDENCE_HISTORY_CHANNEL }) as Promise<VerificationEvidenceHistoryIpcResponse>,
+    loadVerificationEvidencePackage: (packageId: string) => invoke(VERIFICATION_EVIDENCE_LOAD_CHANNEL, { channel: VERIFICATION_EVIDENCE_LOAD_CHANNEL, packageId }) as Promise<VerificationEvidenceLoadIpcResponse>,
   });
 }
